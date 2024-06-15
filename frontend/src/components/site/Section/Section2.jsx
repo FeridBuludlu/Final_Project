633c4b300c865f37f34829c9d0e7f60df1a64c0e
FeridBuludlu <buludluferid@gmail.com>
2024-06-16 02:01:30 +0400

    2header 2section

---
+++ frontend/src/components/site/Section/Section2.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "./Section2.css"
+const Section2 = () => {
+  return (
+    <div className="Section2">
+      <div className="Section2_card1">
+        <img
+          src="http://sneaker.mallthemes.com/wp-content/uploads/2018/10/policy-icon1.png"
+          alt=""
+        />
+        <div className="Section2_card_text">
+          <h4>FREE DELIVERY</h4>
+          <p>Free shipping on all order</p>
+        </div>
+      </div>
+      <div className="Section2_card2">
+        <img
+          src="http://sneaker.mallthemes.com/wp-content/uploads/2018/10/policy-icon2.png"
+          alt=""
+        />
+        <div className="Section2_card_text">
+          <h4>ONLINE SUPPORT 24/7</h4>
+          <p>Support online 24 hours a day</p>
+        </div>
+      </div>
+      <div className="Section2_card3">
+        <img
+          src="http://sneaker.mallthemes.com/wp-content/uploads/2018/10/policy-icon3.png"
+          alt=""
+        />
+        <div className="Section2_card_text">
+          <h4>MONEY RETURN</h4>
+          <p>Back guarantee under 7 days</p>
+        </div>
+      </div>
+      <div className="Section2_card4">
+        <img
+          src="http://sneaker.mallthemes.com/wp-content/uploads/2018/10/policy-icon4.png"
+          alt=""
+        />
+        <div className="Section2_card_text">
+          <h4>MEMBER DISCOUNT</h4>
+          <p>Onevery order over $120.00</p>
+        </div>
+      </div>
+      <div className="Section2_card5">
+        <img
+          src="http://sneaker.mallthemes.com/wp-content/uploads/2018/10/policy-icon5.png"
+          alt=""
+        />
+        <div className="Section2_card_text">
+          <h4>SECURE PAYMENT</h4>
+          <p>All cards accepted</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Section2;
--- frontend/src/pages/site/Home/Home.jsx
+++ frontend/src/pages/site/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import Section1 from '../../../components/site/Section1'
+import Section1 from '../../../components/site/Section/Section1'
+import Section2 from '../../../components/site/Section/Section2'
 
 const Home = () => {
   return (
     <div>
       <Section1/>
+      <Section2/>
     </div>
   )
 }
